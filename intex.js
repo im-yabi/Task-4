6d584b3c566e343b82fc6b6e16e06933d209c40a
im-yabi <151531028+im-yabi@users.noreply.github.com>
2024-08-02 07:22:53 +0530

    Add files via upload

---
+++ intex.js
@@ -0,0 +1,60 @@
+let myResume={
+    "basics": {
+      "name": "YABESH MURUGAN",
+      "email": "YABESHM01@GMAIL.COM",
+      "degree": "MSC",
+      "location": {
+        "address": "NO 44 6TH STREET SVM NAGAR ENNORE ",
+        "postalCode": 600057,
+        "city": "Chennai",
+        "state": "Tamilnadu",
+        "country": "India"
+      },
+      "profiles": [
+        {
+          "website": "https://www.linkedin.com/in/yabesh-murugan-3a8041199/",
+
+        }
+      ]
+    },
+    "work": [
+      {
+        "company": "star health allied insurance co pvt ltd ",
+        "position": "SENIOR CUSTOMER SUPPORT EXCUTIVE (TECHNICAL SUPPORT) ",
+        "startDate": "2021-10-20",
+        "endDate": "STILL NOW",
+        "summary": "TEAM HANDLING AND TEAM MAINTAINING ",
+      },
+    ],
+    "education": [
+      {
+        "institution": "SCP JAIN COLLEGE",
+        "department": "COMPUTER SCIENCE",
+        "studyType": "fulltime",
+        "batch start year": 2017,
+        "batch end year": 2020,
+        "gpa": 7.2,
+      }
+    ],
+    "skills": [
+      {
+        "name": "html,css,php",
+        "level": "intermediate level",
+        "project": [
+          "FOOD AND RESTURANT MANAGEMENT SYSTEM AND STUDENTS MANAGEMENT SYSTEM"
+        ]
+      }
+    ],
+    "languages": [
+      {
+        "language": "Tamil,Enlish",
+      }
+    ],
+    "interests": [
+      {
+        "name": "listing audio podcast and music,books reading,",
+      }
+    ]
+  }
+  console.log(myResume);
+  
